efcaa766a38a8fa7954ff9ebfd79499c3a5c0c87
pool_1541 <pool_1541@hotmail.com>
2023-12-04 19:58:04 -0500

    api endpoint

---
+++ app/api/generate-code-from-image/route.ts
@@ -0,0 +1,99 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+const USER_PROMPT = 'Generate code for a web page that looks exactly like  this';
+const SYSTEM_PROMPT = `You are an expert in designing user interfaces with html and Tailwindcss
+You take screenshots of a reference web page from the user, and then build single page apps 
+using Tailwind, HTML and JS.
+
+- Make sure the app looks exactly like the screenshot.
+- Pay close attention to background color, background color of each component, text color, font size, font family, font weight, line height,
+padding, margin, border, border radius, box shadow, etc. Match the colors and sizes exactly.
+- Make sure each component is exactly the same as the components in the image.
+- Make sure you make responsive design so it looks good on different screen sizes.
+- Make sure the background, text, and icon colors and fill are correct.
+- Make sure the text is exactly the same as the image.
+- Make sure the spacing between elements is correct.
+- Make sure the text is also responsive and adaptable to different screen sizes.
+- Do not add comments in the code such as "<!-- Add other navigation links as needed -->" and "<!-- ... other news items ... -->" in place of writing the full code. WRITE THE FULL CODE.
+- You are also an expert at counting repeated elements in the interface and replicating them, carefully count the number of repeated elements such as cards and write them all in the code to make sure the result is correct.
+- For images, use placeholder images from https://placehold.co and include a detailed description of the image in the alt text so that an image generation AI can generate the image later.
+
+In terms of libraries,
+- Use this script to include Tailwind: <script src="https://cdn.tailwindcss.com"></script>
+- You can use Google Fonts
+- Font Awesome 6 for icons: <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+Return first the background hexadecimals, put a ||| separator, and then all the code.
+Do not include markdown "\`\`\`" or "\`\`\`html" at the start or end.
+Make sure you have the correct code.`;
+
+const REACT_SYSTEM_PROMPT = `You are an expert in designing user interfaces with html and Tailwindcss
+You take screenshots of a reference web page from the user, and then build single page apps 
+using React and Tailwind CSS.
+
+- Make sure the app looks exactly like the screenshot.
+- Pay close attention to background color, background color of each component, text color, font size, font family, font weight, line height,
+padding, margin, border, border radius, box shadow, etc. Match the colors and sizes exactly.
+- Make sure each component is exactly the same as the components in the image.
+- Make sure you make responsive design so it looks good on different screen sizes.
+- Make sure the background, text, and icon colors and fill are correct.
+- Make sure the text is exactly the same as the image.
+- Make sure the spacing between elements is correct.
+- Make sure the text is also responsive and adaptable to different screen sizes.
+- Do not add comments in the code such as "<!-- Add other navigation links as needed -->" and "<!-- ... other news items ... -->" in place of writing the full code. WRITE THE FULL CODE.
+- You are also an expert at counting repeated elements in the interface and replicating them, carefully count the number of repeated elements such as cards and write them all in the code to make sure the result is correct.
+- For images, use placeholder images from https://placehold.co and include a detailed description of the image in the alt text so that an image generation AI can generate the image later.
+
+In terms of libraries,
+
+- Use these script to include React so that it can run on a standalone page:
+    <script src="https://unpkg.com/react/umd/react.development.js"></script>
+    <script src="https://unpkg.com/react-dom/umd/react-dom.development.js"></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.js"></script>
+- Use this script to include Tailwind: <script src="https://cdn.tailwindcss.com"></script>
+- You can use Google Fonts
+- Font Awesome 6 for icons: <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+Return first the background hexadecimals, put a ||| separator, and then all the code.
+Do not include markdown "\`\`\`" or "\`\`\`html" at the start or end.
+Make sure you have the correct code.`;
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { url, img } = await req.json();
+
+  const imageUrl = url ?? img;
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4-vision-preview',
+    stream: true,
+    max_tokens: 4096,
+    messages: [
+      {
+        role: 'system',
+        content: SYSTEM_PROMPT,
+      },
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'text',
+            text: USER_PROMPT,
+          },
+          {
+            type: 'image_url',
+            image_url: { url: imageUrl, detail: 'high' },
+          },
+        ],
+      },
+    ],
+  });
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
