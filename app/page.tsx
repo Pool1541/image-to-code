bc410e0a500adf0ce236d010bb00314bd50d25a9
pool_1541 <pool_1541@hotmail.com>
2023-12-07 22:41:33 -0500

    refactor(page) : refactor code in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import { useState } from 'react';
-import Form from './form';
+import { toast } from 'sonner';
 import { DragAndDrop } from './draganddrop';
+import { Output, OutputType } from '@/types/output.type';
+import { toBase64 } from '@/lib/utils';
 import CodeDisplay from './code-display';
+import Aside from '@/components/layout/aside';
+import Loader from '@/components/ui/loader';
+import Form from './form';
 
 const STEPS = {
   INITIAL: 'INITIAL',
@@ -11,15 +16,6 @@ const STEPS = {
   PREVIEW: 'PREVIEW',
   ERROR: 'ERROR',
 };
-
-function toBase64(file: File) {
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = (error) => reject(error);
-  });
-}
 
 async function* streamReader(res: Response) {
   const reader = res.body?.getReader();
@@ -37,95 +33,71 @@ async function* streamReader(res: Response) {
 export default function Home() {
   const [result, setResult] = useState('');
   const [step, setStep] = useState(STEPS.INITIAL);
+  const [stack, setStack] = useState<OutputType>(Output.html_tailwind);
 
   async function transformToCode(body: string) {
-    setStep(STEPS.LOADING);
-    const res = await fetch('/api/generate-code-from-image', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body,
-    });
+    try {
+      setStep(STEPS.LOADING);
+      const res = await fetch('/api/generate-code-from-image', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body,
+      });
 
-    if (!res.ok || res.body === null) {
-      setStep(STEPS.ERROR);
-      throw new Error('Error al generar el código');
-    }
+      if (!res.ok || res.body === null) {
+        setStep(STEPS.ERROR);
+        const error = await res.json();
+        throw new Error(error.message);
+      }
 
-    setStep(STEPS.PREVIEW);
+      setStep(STEPS.PREVIEW);
 
-    // leer el streaming de datos
-    for await (const chunk of streamReader(res)) {
-      setResult((prev) => prev + chunk);
+      // leer el streaming de datos
+      for await (const chunk of streamReader(res)) {
+        setResult((prev) => prev + chunk);
+      }
+    } catch (error) {
+      if (error instanceof Error) toast.error(error.message);
     }
   }
 
   async function tranformImageToCode(file: File) {
     const img = await toBase64(file);
-    await transformToCode(JSON.stringify({ img }));
+    await transformToCode(JSON.stringify({ img, stack }));
   }
 
   async function transformUrlToCode(url: string) {
-    await transformToCode(JSON.stringify({ url }));
+    await transformToCode(JSON.stringify({ url, stack }));
   }
 
   const [background, ...rest] = result.split('|||');
   const html = rest.pop() || '';
 
   return (
-    <div className='grid sm:grid-cols-[300px_1fr]'>
-      <aside className='flex flex-col justify-between sm:min-h-screen p-4 bg-gray-900'>
-        <header className='text-center'>
-          <h1 className='text-2xl font-semibold'>Image 2 code</h1>
-          <h2 className='text-sm opacity-75 mt-2'>Crea componentes a partir de imágenes</h2>
-        </header>
-
-        <section>{/*Aquí irán los filtros.*/}</section>
-
-        <footer>Desarrollado por Pool Llerena</footer>
-      </aside>
+    <div className='grid lg:grid-cols-[350px_1fr]'>
+      <Aside handleChange={(value: OutputType) => setStack(value)} />
       <main className='bg-gray-950'>
-        <section className='max-w-7xl w-full mx-auto p-10'>
-          {step === STEPS.LOADING && (
-            <div className='flex justify-center items-center'>
-              <div role='status'>
-                <svg
-                  aria-hidden='true'
-                  className='w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
-                  viewBox='0 0 100 101'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'>
-                  <path
-                    d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-                    fill='currentColor'
-                  />
-                  <path
-                    d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-                    fill='currentFill'
-                  />
-                </svg>
-                <span className='sr-only'>Loading...</span>
-              </div>
-            </div>
-          )}
-
+        <section className='max-w-7xl w-full min-h-screen mx-auto p-10 flex justify-center'>
+          {step === STEPS.LOADING && <Loader />}
           {step === STEPS.INITIAL && (
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-4 w-full'>
               <DragAndDrop tranformImageToCode={tranformImageToCode} />
               <Form transformUrlToCode={transformUrlToCode} />
             </div>
           )}
           {step === STEPS.PREVIEW && (
-            <div className='rounded border flex flex-col gap-4'>
+            <div className='rounded flex flex-col gap-4'>
               <div
                 className='w-full h-full border rounded-4 border-gray-700  aspect-video'
                 style={{ backgroundColor: `#${background ? background : 'fff'}` }}>
-                <iframe srcDoc={html} className='w-full h-full' />
+                <iframe srcDoc={html} className='w-full h-[832px]' />
               </div>
               <CodeDisplay>{html}</CodeDisplay>
             </div>
           )}
+          {step === STEPS.ERROR && <Loader />}
         </section>
       </main>
     </div>
