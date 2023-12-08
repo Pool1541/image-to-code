87d8819a6593eb594b29acfdf8ad976cc4255f86
pool_1541 <pool_1541@hotmail.com>
2023-12-07 22:39:01 -0500

    chore(types): add output type

---
+++ types/output.type.ts
@@ -0,0 +1,6 @@
+export type OutputType = 'html-tailwind' | 'react-tailwind';
+
+export const Output = {
+  html_tailwind: 'html-tailwind' as OutputType,
+  react_tailwind: 'react-tailwind' as OutputType,
+};
