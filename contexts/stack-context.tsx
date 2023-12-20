632bbf0b3445249112ec4af40bf819816801e3b6
pool_1541 <pool_1541@hotmail.com>
2023-12-19 22:29:16 -0500

    refactor(context): Improve stackContext

--- contexts/stack-context.tsx
+++ contexts/stack-context.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import { Output, OutputType } from '@/types/output.type';
 import { createContext, useState } from 'react';
 
-export const StackContext = createContext<any>(null);
+export const StackContext = createContext<{
+  stack: OutputType;
+  setStack: React.Dispatch<React.SetStateAction<OutputType>>;
+}>({
+  stack: Output.html_tailwind,
+  setStack: () => {},
+});
 
 export default function StackProvider({ children }: { children: React.ReactNode }) {
-  const [stack, setStack] = useState<any>('Tailwind + React');
+  const [stack, setStack] = useState<OutputType>(Output.html_tailwind);
 
   return <StackContext.Provider value={{ stack, setStack }}>{children}</StackContext.Provider>;
 }
