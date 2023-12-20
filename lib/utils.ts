b05b9e309d5fc6ce989ac1e77e8eceb8db2418cf
pool_1541 <pool_1541@hotmail.com>
2023-12-19 22:09:37 -0500

    chore(utils): add streamReader function

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,3 +22,16 @@ export function toBase64(file: File) {
     reader.onerror = (error) => reject(error);
   });
 }
+
+export async function* streamReader(res: Response) {
+  const reader = res.body?.getReader();
+  const decoder = new TextDecoder();
+  if (reader == null) return;
+
+  while (true) {
+    const { done, value } = await reader.read();
+    const chunk = decoder.decode(value);
+    yield chunk;
+    if (done) break;
+  }
+}
