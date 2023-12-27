62e154dbbabf3f343675cb249a3ee183f49dfde8
pool_1541 <pool_1541@hotmail.com>
2023-12-26 23:20:06 -0500

    feat(components): update error view in mainContent component

--- components/home/main-content.tsx
+++ components/home/main-content.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
 import Loader from '@/components/ui/loader';
@@ -6,9 +7,25 @@ import { DragAndDrop } from './draganddrop';
 import Form from './form';
 import CodeDisplay from './code-display';
 import { STEPS } from '@/constants/steps';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 
 export default function MainContent() {
   const { background, html, step, tranformImageToCode, transformUrlToCode } = useToCode();
+  // const { data } = useSession();
+
+  // useEffect(() => {
+  //   async function getFreeTrial() {
+  //     if (data?.user && 'id' in data.user) {
+  //       const res = await fetch('/api/free-trial/' + data.user.id);
+  //       const info = await res.json();
+  //       console.log(info);
+  //     }
+  //   }
+
+  //   getFreeTrial();
+  // }, [data]);
+
   return (
     <section className='max-w-7xl w-full mx-auto mt-10 flex justify-center'>
       {step === STEPS.LOADING && <Loader />}
@@ -28,7 +45,12 @@ export default function MainContent() {
           <CodeDisplay>{html}</CodeDisplay>
         </div>
       )}
-      {step === STEPS.ERROR && <Loader />}
+      {step === STEPS.ERROR && (
+        <div className='flex flex-col gap-4 w-full'>
+          <DragAndDrop tranformImageToCode={tranformImageToCode} />
+          <Form transformUrlToCode={transformUrlToCode} />
+        </div>
+      )}
     </section>
   );
 }
