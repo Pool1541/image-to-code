34f013ae3a25840d815b801c739096e344f2140b
pool_1541 <pool_1541@hotmail.com>
2024-01-05 00:16:43 -0500

    feat(page): Add FeedBack component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 import Aside from '@/components/layout/aside';
 import Navbar from '@/components/layout/navbar';
 import MainContent from '../components/home/main-content';
+import { Button } from '@/components/ui/button';
+import FeedbackForm from '@/components/home/feedback-form';
 
 export default function Home() {
   return (
-    <div className='grid lg:grid-cols-[350px_1fr]'>
-      <Aside />
-      <main className='bg-gray-950 p-10'>
-        <header className='flex justify-between items-middle pb-5'>
-          <h2>Dashboard</h2>
-          <nav>
-            <Navbar />
-          </nav>
-        </header>
-        <MainContent />
-      </main>
-    </div>
+    <>
+      <div className='lg:grid grid-cols-[350px_1fr]'>
+        <Aside />
+        <main className='bg-gray-950 p-2 md:p-10 min-h-screen'>
+          <header className='flex justify-between items-middle pb-5'>
+            <h2>Dashboard</h2>
+            <nav>
+              <Navbar />
+            </nav>
+          </header>
+          <MainContent />
+        </main>
+      </div>
+      <FeedbackForm />
+    </>
   );
 }
