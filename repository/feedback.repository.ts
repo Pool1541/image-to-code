9dba494d11a18b2e340a6a8296f38197d4e8fbfa
pool_1541 <pool_1541@hotmail.com>
2024-01-05 00:08:36 -0500

    fix(repository): Fix feedBack repository

--- repository/feedback.repository.ts
+++ repository/feedback.repository.ts
@@ -8,11 +8,12 @@ export class FeedBackRepository {
     return await this.instance.feedback.findMany();
   }
 
-  async findOne(id: string) {
-    return await this.instance.feedback.findUnique({
-      where: { userId: id },
-    });
-  }
+  // findUnique no funciona porque se necesita hacer la busqueda por id o algún valor unique. El valor userId de Feedback no es único ya que tiene una relación uno a muchos con User.
+  // async findOne(id: string) {
+  //   return await this.instance.feedback.findUnique({
+  //     where: { userId: id },
+  //   });
+  // }
 
   async create(data: Prisma.FeedbackUncheckedCreateInput) {
     return await this.instance.feedback.create({
