ff990f088b8caae271ec0af1458fe47e87bce2b4
pool_1541 <pool_1541@hotmail.com>
2024-01-19 22:34:25 -0500

    chore(Component): Add ExclamationIcon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as ClipboardIcon } from './Clipboard.icon';
 export { default as GoogleIcon } from './Google.icon';
 export { default as Xicon } from './x.icon';
 export { default as MenuIcon } from './Menu.icon';
+export { default as ExclamationIcon } from './exclamation.icon';
