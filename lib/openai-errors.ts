a78a42a06dea604ccf9a739acaacdda2b2f239b0
pool_1541 <pool_1541@hotmail.com>
2024-01-19 22:36:06 -0500

    chore(Lib): Update OPENAI_ERRORS constant

--- lib/openai-errors.ts
+++ lib/openai-errors.ts
@@ -4,4 +4,5 @@ interface OPENAI_ERROR_INTERFACE {
 
 export const OPENAI_ERRORS: OPENAI_ERROR_INTERFACE = {
   401: 'Ingresa una api key válida en la configuración.',
+  404: 'Tu cuenta de openai no tiene acceso al modelo necesario para usar la aplicación. Por favor sigue las instrucciones del siguiente enlace para activarlo: https://help.openai.com/en/articles/7102672-how-can-i-access-gpt-4.',
 };
