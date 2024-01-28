4ada05c86be9632956823d4e46b910b6e77d56ae
pool_1541 <pool_1541@hotmail.com>
2024-01-27 21:49:59 -0500

    feat(lib): Add logtail to logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,5 +1,27 @@
 import { createLogger, format, transports } from 'winston';
+import { Logtail } from '@logtail/node';
+import { LogtailTransport } from '@logtail/winston';
 import 'winston-daily-rotate-file';
+
+const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN!);
+
+const customFormat = ({ isConsole = false }: { isConsole?: Boolean } = {}) => {
+  const formatBase = [
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.printf(
+      ({ level, message, label = process.env.NODE_ENV, timestamp }) =>
+        `${timestamp} [${label}] ${level}: ${message}`
+    ),
+  ];
+
+  if (isConsole) return format.combine(format.colorize(), ...formatBase);
+
+  return format.combine(...formatBase);
+};
 
 const getLogger = (fileName = 'application') => {
   const fileLogTransport = new transports.DailyRotateFile({
@@ -8,36 +30,25 @@ const getLogger = (fileName = 'application') => {
     zippedArchive: true,
     maxSize: '20m',
     maxFiles: '30d',
+    format: customFormat(),
   });
 
   const consoleTransport = new transports.Console({
-    level: process.env.LOG_LEVEL || 'info',
+    level: 'info',
     handleExceptions: false,
-    format: format.combine(
-      format.colorize(),
-      format.printf((i) => `${i.message}`)
-    ),
+    format: customFormat({ isConsole: true }),
   });
 
   const logger = createLogger({
     level: 'info',
-    format: format.combine(
-      format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-      }),
-      format.errors({ stack: true }),
-      format.splat(),
-      format.printf(
-        ({ level, message, label = process.env.NODE_ENV, timestamp }) =>
-          `${timestamp} [${label}] ${level}: ${message}`
-      )
-    ),
-    defaultMeta: { service: 'my-app' },
+    defaultMeta: { service: 'image-to-code' },
     transports: [consoleTransport],
   });
 
   if (process.env.NODE_ENV === 'development') {
     logger.add(fileLogTransport);
+  } else {
+    logger.add(new LogtailTransport(logtail));
   }
 
   return logger;
